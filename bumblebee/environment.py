08a6d3742262a6e7b41a4b367ba952a16924cdd3
Pay Giesselmann <pay.giesselmann@web.de>
2021-06-08 20:14:31 +0200

    param updates

--- bumblebee/environment.py
+++ bumblebee/environment.py
@@ -150,7 +150,8 @@ class EnvReadEvents():
         next_state = self.__get_state__()
         next_action = self.__get_next_action__()
         done = (self.seq_step_idx == self.target_seq_len or
-                self.matches / (self.seq_step_idx or 1) < 0.5 or
+                    (self.seq_step_idx > 20 and
+                    self.matches / (self.seq_step_idx or 1) < 0.5) or
                 done)
         info = {
             'matches': self.matches / (self.seq_step_idx or 1),
