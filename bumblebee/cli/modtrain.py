a2deea2bbd4b0867560ed16cc23559bbf3512b73
Pay Giesselmann <pay.giesselmann@web.de>
2021-06-28 16:32:31 +0200

    config updates

--- bumblebee/cli/modtrain.py
+++ bumblebee/cli/modtrain.py
@@ -169,8 +169,8 @@ def main(args):
         lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
             mode='max',
             factor=0.5,
-            patience=len(dl_train)//4,
-            cooldown=len(dl_train))
+            patience=len(dl_train),
+            cooldown=2*len(dl_train))
     # load checkpoint
     chkpt_file = os.path.join(args.prefix, 'latest.chkpt')
     out_file = os.path.join(args.prefix, 'final.chkpt')
